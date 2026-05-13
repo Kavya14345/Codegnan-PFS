d02d1e0b02df31cd1317afd59df18eb8024c12fe
Kavya14345 <117656142+Kavya14345@users.noreply.github.com>
2026-05-13 15:27:35 +0530

    Add files via upload

---
+++ Codegnan/day1.py
@@ -0,0 +1,76 @@
+print("Hello")
+'''
+What is python?
+1. High level language
+   -> no need to worry about low-level management
+2. Interpreter language
+   -> line by line execution and can throw the error if any but all before lines will execute and gives output.
+3. Dynamically typed language
+   -> No need to mention the kind of data taking to variable
+
+'''
+num=23
+so="python"
+any=[1,2]
+an={"Name":"Kavya"}
+print(num)
+print(so)
+print(any)
+print(an)
+
+'''
+Tokens- keywords in the program
+eg: while,for,if,else etc
+
+Identifiers- name of variables,function and classes
+
+Variables- acts as containers to store the value
+Good ways:
+A-Z,a-z,_
+Bad ways:
+Special characters, space, numbers
+'''
+a=90
+A=100
+_s=89
+
+a={"Name":"kavya"}
+b={"Name":"kanchana"}
+c={"Name":"renuka"}
+
+hdfc_cus_details={"Name":"Sai"}
+sbi_cus_details={"Name":"kalavathi"}
+icici_cus_details={"Name":"Deepu"}
+
+print(a,b,c)
+print(hdfc_cus_details,sbi_cus_details,icici_cus_details)
+
+'''
+Comments
+--------
+1.single line(#)
+2.Multi-line(''' ''')
+
+'''
+
+'''
+Create 3 variables of different datatypes
+'''
+#Student details
+stu_id=45
+#9stu_name="kavya"
+#st$u_cgpa=7.9
+#s t u= 46
+
+'''
+
+boolean
+-------
+True or False
+'''
+num1=9
+num2=7
+print(num1==num2)
+
+
+
