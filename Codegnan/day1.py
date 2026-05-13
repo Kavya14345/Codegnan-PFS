6c79cfa0ef5a17be3516f8168c5c2449c41e3fd7
Kavya14345 <117656142+Kavya14345@users.noreply.github.com>
2026-05-13 14:53:31 +0530

    Add files via upload

---
+++ Codegnan/day1.py
@@ -0,0 +1,19 @@
+print("Hello")
+'''
+What is python?
+1. High level language
+   -> no need to worry about low-level management
+2. Interpreter language
+   -> line by line execution and can throw the error if any but all before lines will execute and gives output.
+3. Dynamically typed language
+   -> No need to mention the kind of data taking to variable
+
+'''
+num=23
+so="python"
+any=[1,2]
+an={"Name":"Kavya"}
+print(num)
+print(so)
+print(any)
+print(an)
